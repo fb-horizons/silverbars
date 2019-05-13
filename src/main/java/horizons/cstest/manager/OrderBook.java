81e8f59a82499a4839379140b82a09d49b333e0a
fb-horizons <faisal_bin_bashir@yahoo.com>
2019-05-13 15:39:43 +0100

    Added demo methods for displaying Live Order Board

--- src/main/java/horizons/cstest/manager/OrderBook.java
+++ src/main/java/horizons/cstest/manager/OrderBook.java
@@ -11,6 +11,9 @@ import java.util.Map;
 import java.util.Comparator;
 import java.util.Collections;
 
+import static horizons.cstest.model.OrderSide.BUY;
+import static horizons.cstest.model.OrderSide.SELL;
+
 public class OrderBook {
     private final List<Order> orders = new ArrayList<>();
     /*
@@ -108,6 +111,59 @@ public class OrderBook {
                                                     aggregatedOrder.getQuantity() - order.getQuantity()));
             }
         }
+    }
+
+    /* following is for demo purposes only */
+    public static void main(String[] args){
+        Order sellOrder1, sellOrder2, sellOrder3, sellOrder4;
+        Order buyOrder1, buyOrder2, buyOrder3, buyOrder4;
+
+        sellOrder1 = new Order(SELL, 306, 3.5, "user1");
+        sellOrder2 = new Order(SELL, 310, 1.2, "user2");
+        sellOrder3 = new Order(SELL, 307, 4.0, "user1");
+        sellOrder4 = new Order(SELL, 306, 1.5, "user3");
+
+        buyOrder1 = new Order(BUY, 305, 3.5, "user1");
+        buyOrder2 = new Order(BUY, 307, 4.0, "user1");
+        buyOrder3 = new Order(BUY, 305, 2.5, "user3");
+        buyOrder4 = new Order(BUY, 304, 6.2, "user2");
+
+        OrderBook orderBook = new OrderBook();
+        orderBook.addOrder(sellOrder1);
+        System.out.println("Added " + sellOrder1);
+        orderBook.addOrder(sellOrder2);
+        System.out.println("Added " + sellOrder2);
+        orderBook.addOrder(sellOrder3);
+        System.out.println("Added " + sellOrder3);
+        orderBook.addOrder(buyOrder1);
+        System.out.println("Added " + buyOrder1);
+        orderBook.addOrder(buyOrder2);
+        System.out.println("Added " + buyOrder2);
+        orderBook.addOrder(buyOrder4);
+        System.out.println("Added " + buyOrder4);
+        printOrderBookStatus(orderBook);
+
+        orderBook.addOrder(sellOrder4);
+        System.out.println("Added " + sellOrder4);
+        orderBook.addOrder(buyOrder3);
+        System.out.println("Added " + buyOrder3);
+        printOrderBookStatus(orderBook);
+
+        orderBook.cancelOrder(sellOrder1);
+        System.out.println("Removed " + sellOrder1);
+        orderBook.cancelOrder(buyOrder1);
+        System.out.println("Removed " + buyOrder1);
+        printOrderBookStatus(orderBook);
+
+    }
+
+    private static void printOrderBookStatus(OrderBook orderBook){
+        System.out.println("\nLive Order Board");
+        System.out.println("BUY ORDERS");
+        orderBook.getAggregatedBuyOrders().stream().forEach(System.out::println);
+        System.out.println("SELL ORDERS");
+        orderBook.getAggregatedSellOrders().stream().forEach(System.out::println);
+        System.out.println("END\n");
     }
 }
 
